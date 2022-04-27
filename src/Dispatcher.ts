d9f4d403b63c2d6722e7568b2d0d1fa7c04c6071
hirokihello <iammyeye1@gmail.com>
2022-04-27 19:04:04 +0900

    dispatcherにregisterするメソッドを追加

--- src/Dispatcher.ts
+++ src/Dispatcher.ts
@@ -1,16 +1,23 @@
 export class Dispatcher {
-  store_callbacks: any;
+  _store_callbacks: any;
+  _last_store_id: number;
 
   constructor() {
-    this.store_callbacks = {};
+    this._store_callbacks = {};
+    this._last_store_id = -1;
+  }
+
+  register(callback_fn: Function): void {
+    this._last_store_id++;
+    this._store_callbacks[this._last_store_id] = callback_fn;
   }
 
   //payload
   // { action_name: string, ...args }
   dispatch(payload: any) {
     // 登録された全てのcallbackを実行する
-    for(let id in this.store_callbacks) {
-      this.store_callbacks[id](payload);
+    for(let id in this._store_callbacks) {
+      this._store_callbacks[id](payload);
     }
   }
 }
